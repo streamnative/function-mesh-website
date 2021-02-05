dd93a7a030d703e349ea3d69430f7625bd3a881a
HuanliMeng <48120384+Huanli-Meng@users.noreply.github.com>
2021-02-05 14:30:48 +0800

    update sidebar doc

--- sidebars.js
+++ sidebars.js
@@ -1,6 +1,22 @@
-module.exports = {
-  someSidebar: {
-    Docusaurus: ['doc1', 'doc2', 'doc3'],
-    Features: ['mdx'],
-  },
-};
+{
+  "data": [
+    {
+      "level": 1,
+      "key": "/overview.md",
+      "label": "Overview",
+      "path": "/docs/overview",
+      "children": [],
+      "active": false,
+      "id": 1
+    },
+    {
+      "level": 1,
+      "key": "/install-function-mesh.md",
+      "label": "Installation",
+      "path": "/docs/install-function-mesh",
+      "children": [],
+      "active": false,
+      "id": 2
+    }
+  ]
+}

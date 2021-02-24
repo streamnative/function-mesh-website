1c3803786fd06ee8c99f9b317a038caa62a51aeb
Huanli Meng <48120384+Huanli-Meng@users.noreply.github.com>
2021-02-25 01:02:45 +0800

    update contents on Function Mesh website (#12)

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,28 +12,24 @@ const features = [
     imageUrl: 'img/undraw_docusaurus_mountain.svg',
     description: (
       <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
+        <ul>
+          <li>Be easily deployed directly on Kubernetes clusters, including{' '} <a href="https://github.com/kubernetes/minikube">Minikube</a> and{' '} <a href="https://kind.sigs.k8s.io/docs/user/quick-start/">Kind</a>.</li>
+          <li>Allow one function to talk to multiple different Pulsar clusters, which are defined as config maps.</li>
+          <li>Support function registry for function package management. We are going to introduce the Pulsar registry in Pulsar 2.8.0 for function package management. Then, the function package can be reused by different functions.</li>
+        </ul>
       </>
     ),
   },
   {
-    title: 'Focus on What Matters',
+    title: 'Integrated with Kubernetes',
     imageUrl: 'img/undraw_docusaurus_tree.svg',
     description: (
       <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
-      </>
-    ),
-  },
-  {
-    title: 'Powered by React',
-    imageUrl: 'img/undraw_docusaurus_react.svg',
-    description: (
-      <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
+        <ul>
+          <li>Integrate with Kubernetes secrets seamlessly to read secrets directly.</li>
+          <li>Leverage the Kubernetes’s auto-scaler to auto-scale instances for functions based on the CPU usage. In future, Function Mesh will support auto-scaling based on the backlog.</li>
+          <li>Utilize the full power scheduling capability provided by Kubernetes. Therefore, users do not need to write any customized codes to communicate with the Kubernetes API server.</li>
+        </ul>
       </>
     ),
   },
@@ -59,7 +55,7 @@ function Home() {
   const {siteConfig = {}} = context;
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
+      title={`A collection of functions collaborated to process data on Kubernetes`}
       description="Description will go into a meta tag in <head />">
       <header className={clsx('hero hero--primary', styles.heroBanner)}>
         <div className="container">

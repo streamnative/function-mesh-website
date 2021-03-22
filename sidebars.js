491ab9901887f0e223a8daf2d03d46168dceef9a
HuanliMeng <48120384+Huanli-Meng@users.noreply.github.com>
2021-03-22 15:07:24 +0800

    update contents

--- sidebars.js
+++ sidebars.js
@@ -2,6 +2,6 @@
 module.exports = {
   someSidebar: {
     Docusaurus: ['overview', 'install-function-mesh', 'doc3'],
-    Configure: ['pulsar-functions', 'pulsar-connector', 'function-mesh'],
+    Configuration: ['pulsar-functions', 'pulsar-connector', 'function-mesh'],
   },
 };

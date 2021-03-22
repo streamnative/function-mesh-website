fbb1858db6f10b693a72c32cb8d38a80b730e0d2
HuanliMeng <48120384+Huanli-Meng@users.noreply.github.com>
2021-03-22 15:05:23 +0800

    Updatesidebar

--- sidebars.js
+++ sidebars.js
@@ -1,15 +1,7 @@
+
 module.exports = {
-  docs: [
-    'overview',
-    'install-function-mesh',
-    {
-      type: 'category',
-      label: 'Configuration',
-      items: [
-        'configuration/pulsar-functions', 
-        'configuration/pulsar-connector', 
-        'configuration/function-mesh',
-      ],
-    },
-  ],
-}
+  someSidebar: {
+    Docusaurus: ['overview', 'install-function-mesh', 'doc3'],
+    Configure: ['pulsar-functions', 'pulsar-connector', 'function-mesh'],
+  },
+};

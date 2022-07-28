95d18fd58a5a1821fbebb7ae9e6e639fd0f00887
Li Li <urfree@apache.org>
2022-07-28 16:40:49 +0800

    feat: add google analytics support (#148)

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,70 +1,71 @@
 module.exports = {
-  title: 'Function Mesh',
-  tagline: 'The serverless framework purpose-built for event streaming applications',
-  url: 'https://functionmesh.io/',
-  baseUrl: '/',
-  onBrokenLinks: 'throw',
-  favicon: 'img/favicon.ico',
-  organizationName: 'function-mesh', // Usually your GitHub org/user name.
-  projectName: 'functionmesh.io/', // Usually your repo name.
+  title: "Function Mesh",
+  tagline:
+    "The serverless framework purpose-built for event streaming applications",
+  url: "https://functionmesh.io/",
+  baseUrl: "/",
+  onBrokenLinks: "throw",
+  favicon: "img/favicon.ico",
+  organizationName: "function-mesh", // Usually your GitHub org/user name.
+  projectName: "functionmesh.io/", // Usually your repo name.
   themeConfig: {
     colorMode: {
       disableSwitch: true,
     },
     navbar: {
       logo: {
-        alt: 'FUnction Mesh Logo',
-        src: 'img/logo.svg',
+        alt: "FUnction Mesh Logo",
+        src: "img/logo.svg",
       },
       items: [
         {
-          type: 'docsVersionDropdown',
-          position: 'left',
+          type: "docsVersionDropdown",
+          position: "left",
         },
         {
-          to: 'docs/',
-          activeBasePath: 'docs',
-          label: 'Documentation',
-          position: 'left',
+          to: "docs/",
+          activeBasePath: "docs",
+          label: "Documentation",
+          position: "left",
         },
         {
-          href: 'https://github.com/streamnative/function-mesh',
-          label: 'GitHub',
-          position: 'right',
+          href: "https://github.com/streamnative/function-mesh",
+          label: "GitHub",
+          position: "right",
         },
       ],
     },
     footer: {
-      style: 'dark',
+      style: "dark",
       links: [
         {
-          title: 'Documentation',
+          title: "Documentation",
           items: [
             {
-              label: 'Installation Guide',
-              to: 'https://functionmesh.io/docs/install-function-mesh',
+              label: "Installation Guide",
+              to: "https://functionmesh.io/docs/install-function-mesh",
             },
             {
-              label: 'Configuration Guide',
-              to: 'https://functionmesh.io/docs/function-mesh/run-function-mesh',
+              label: "Configuration Guide",
+              to: "https://functionmesh.io/docs/function-mesh/run-function-mesh",
             },
           ],
         },
         {
-          title: 'Community',
+          title: "Community",
           items: [
             {
-              label: 'Email',
-              href: 'mailto:function-mesh@streamnative.io',
-            }
+              label: "Email",
+              href: "mailto:function-mesh@streamnative.io",
+            },
           ],
         },
         {
-          title: 'More',
+          title: "More",
           items: [
             {
-              label: 'GitHub',
-              href: 'https://github.com/streamnative/function-mesh',
+              label: "GitHub",
+              href: "https://github.com/streamnative/function-mesh",
             },
           ],
         },
@@ -72,23 +73,28 @@ module.exports = {
       copyright: `Copyright © ${new Date().getFullYear()} StreamNative, Inc.`,
     },
     algolia: {
-      apiKey: '06b1278e604c2d3e61ac0410591143ed',
-      indexName: 'functionmesh',
-      contextualSearch: false
-    }
+      appId: "BH4D9OD16A",
+      apiKey: "06b1278e604c2d3e61ac0410591143ed",
+      indexName: "functionmesh",
+      contextualSearch: false,
+    },
   },
   presets: [
     [
-      '@docusaurus/preset-classic',
+      "@docusaurus/preset-classic",
       {
         docs: {
-          sidebarPath: require.resolve('./sidebars.js'),
+          sidebarPath: require.resolve("./sidebars.js"),
           // Please change this to your repo.
           editUrl:
-            'https://github.com/streamnative/function-mesh-website/tree/main',
+            "https://github.com/streamnative/function-mesh-website/tree/main",
         },
         theme: {
-          customCss: require.resolve('./src/css/custom.css'),
+          customCss: require.resolve("./src/css/custom.css"),
+        },
+        gtag: {
+          trackingID: "G-FPJBNXYJQV",
+          anonymizeIP: true,
         },
       },
     ],
